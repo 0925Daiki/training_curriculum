11a5f728eb99a8f8247fda55dcd68179192e7bcc
0925Daiki <82555115+0925Daiki@users.noreply.github.com>
2021-04-23 18:01:26 +0900

    first commit

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,11 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
+<<<<<<< HEAD
     get_week
+=======
+    getWeek
+>>>>>>> parent of 44c54eba (Revert "first_commit")
     @plan = Plan.new
   end
 
@@ -18,7 +22,11 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
+<<<<<<< HEAD
   def get_week
+=======
+  def getWeek
+>>>>>>> parent of 44c54eba (Revert "first_commit")
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -34,7 +42,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+<<<<<<< HEAD
       days = { :month: (@todays_date + x).month, :date: (@todays_date+x).day, :plans: today_plans}
+=======
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+>>>>>>> parent of 44c54eba (Revert "first_commit")
       @week_days.push(days)
     end
 
